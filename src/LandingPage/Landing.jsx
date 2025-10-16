4413a599904699135059f8a4df17a4f1f8bdc784
iyyneswaran <pdee5660@gmail.com>
2025-10-16 22:32:27 +0530

    fix

--- src/LandingPage/Landing.jsx
+++ src/LandingPage/Landing.jsx
@@ -29,7 +29,7 @@ const Landing = () => {
     return (
         <div className={styles.container}>
             <h1><span>Hi👋🏻 I'm Iyynes! </span>Welcome to my practice world!</h1>
-            <p>Click over it and visit look over my works</p>
+            <p>Click over it and look over my works</p>
             <div className={styles.projectContainer}>
                 {pages.map((item, index) => (
                     <div key={index} onClick={() => navigate(item.route)}>
